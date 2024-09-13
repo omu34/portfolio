22409fed5fea616acf5e49f4fb47652fc314df80
omu34 <bernardomuse22@gmail.com>
2024-09-13 16:05:21 +0300

    changes

---
+++ js/3-D.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            keyframes: {
+                rotate: {
+                    '0%': { transform: 'perspective(1000px) rotateY(0deg)' },
+                    '100%': { transform: 'perspective(1000px) rotateY(360deg)' }
+                }
+            },
+            animation: {
+                rotate: 'rotate 180s linear infinite',
+            }
+        }
+    }
+}
