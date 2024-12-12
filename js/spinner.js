abebecf193560ad191f48757274598571585d148
omu34 <bernardomuse22@gmail.com>
2024-12-12 13:26:31 +0300

    tailwind bundled

--- js/spinner.js
+++ js/spinner.js
@@ -2,4 +2,4 @@ var spinner = document.getElementById("spinner");
 spinner.style.animation = "spin 2s linear infinite";
 setTimeout(function() {
     window.location.href = "home.html"
-}, 9000);
+}, 4000);
